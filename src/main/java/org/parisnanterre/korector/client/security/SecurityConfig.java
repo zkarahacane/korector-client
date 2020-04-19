0ee1998dc16a84cd2c9fa41a3a73d98b59b564c8
zkarahacane <karahacane.zakari@gmail.com>
2020-04-20 00:59:05 +0200

    commit

--- src/main/java/org/parisnanterre/korector/client/security/SecurityConfig.java
+++ src/main/java/org/parisnanterre/korector/client/security/SecurityConfig.java
@@ -1,22 +1,86 @@
 package org.parisnanterre.korector.client.security;
 
 
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-public class SecurityConfig   {
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // Validate tokens through configured OpenID Provider
+        http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter());
+        // Require authentication for all requests
+        http.authorizeRequests().anyRequest().authenticated();
+        // Allow showing pages within a frame
+        http.headers().frameOptions().sameOrigin();
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        // Convert realm_access.roles claims to granted authorities, for use in access decisions
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter());
+        return jwtAuthenticationConverter;
+    }
 
     @Bean
-    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) throws Exception {
-        http
-                .authorizeExchange()
-                .pathMatchers("/resource")
-                .authenticated()
-                .and()
-                .oauth2ResourceServer()
-                .jwt();
-        return http.build();
+    public JwtDecoder jwtDecoderByIssuerUri(OAuth2ResourceServerProperties properties) {
+        String issuerUri = properties.getJwt().getIssuerUri();
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromIssuerLocation(issuerUri);
+        // Use preferred_username from claims as authentication name, instead of UUID subject
+        jwtDecoder.setClaimSetConverter(new UsernameSubClaimAdapter());
+        return jwtDecoder;
     }
+
 }
 
+// As per: https://docs.spring.io/spring-security/site/docs/5.2.x/reference/html5/#oauth2resourceserver-jwt-claimsetmapping-rename
+class UsernameSubClaimAdapter implements Converter<Map<String, Object>, Map<String, Object>> {
+
+    private final MappedJwtClaimSetConverter delegate = MappedJwtClaimSetConverter.withDefaults(Collections.emptyMap());
+
+    @Override
+    public Map<String, Object> convert(Map<String, Object> claims) {
+        Map<String, Object> convertedClaims = this.delegate.convert(claims);
+        String username = (String) convertedClaims.get("preferred_username");
+        convertedClaims.put("sub", username);
+        return convertedClaims;
+    }
+
+}
+
+class KeycloakRealmRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Collection<GrantedAuthority> convert(final Jwt jwt) {
+        final Map<String, Object> realmAccess = (Map<String, Object>) jwt.getClaims().get("realm_access");
+        return ((List<String>) realmAccess.get("roles")).stream()
+                .map(roleName -> "ROLE_" + roleName)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+}
